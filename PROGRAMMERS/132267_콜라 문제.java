801717b8e7d155bdb08a4b2d36fed1fd3b007b34
BruceHan <intragerhan@gmail.com>
2025-12-07 19:11:58 +0900

    Java / 0.015714285714285715ms

--- PROGRAMMERS/132267_콜라 문제.java
+++ PROGRAMMERS/132267_콜라 문제.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        int answer = 0;
-        while (n >= a) {
-            n = n - a + b;
-            answer += b;
-        }
-        return answer;
+        return (n > b ? n - b : 0) / (a - b) * b;
     }
 }
